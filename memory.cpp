9313134d0273de77230869e4fdc5339754b15bce
Joshua Mashburn <joshualmashburn@tamu.edu>
2022-03-29 00:45:35 -0500

    Implemented memory controller. Needs integration

---
+++ memory.cpp
@@ -0,0 +1,135 @@
+//
+// Created by joshua on 3/29/22.
+//
+
+#include "memory.h"
+
+memory::memory() {
+    //load boot rom
+    //open file as binary stream and move file pointer to end
+    std::ifstream file("mgb_boot.bin", std::ios::binary | std::ios::ate);
+    if(file.is_open())
+    {
+        char* buffer = new char[256];
+
+        //go back to beginning of file and fill buffer
+        file.seekg(0, std::ios::beg);
+        file.read(buffer, 256);
+        file.close();
+
+        //load ROM contents into the gb's memory, starting at 0x0
+        for (int i = 0; i < 256; i++)
+        {
+            bootrom[i] = buffer[i];
+        }
+
+        //free the buffer
+        delete[] buffer;
+    }
+}
+
+void memory::write_mem(uint16_t address, uint8_t value) {
+//todo: implement rom banking, any other indirection, etc.
+    //todo: go back and replace all the memory[] writes
+    //don't allow writes to read-only memory
+    if (address < 0x8000)
+    {
+        printf("Attempted write to ROM, address %x\n", address);
+    }
+        // writes to ECHO RAM writes to RAM as well
+    else if ((address >= 0xE000) && (address < 0xFE00))
+    {
+        system_mem[address] = value;
+        write_mem(address-0x2000, value);
+    }
+        //this area is restricted
+    else if (( address >= 0xFEA0 ) && (address < 0xFEFF))
+    {
+        printf("Attempted write to restricted memory, address %x\n", address);
+    }
+    else if (address == 0xFF07)
+    {
+        //update timer controller!
+        system_mem[0xFF07] = value;
+        set_clock_freq();
+    }
+    else if (address == 0xFF04){
+        //attempted write to timer div register, resets it
+        system_mem[0xFF04] = 0;
+    }
+    else if (address == 0xFF44)
+    {
+        //attempted write to scanline counter, resets it
+        system_mem[address] = 0;
+    }
+    else if (address == 0xFF46)
+    {
+        //DMA time
+        performDMAtransfer(value);
+    }
+    else
+        system_mem[address] = value;
+}
+
+uint8_t memory::read_mem(uint16_t address) {
+    //todo: go back and replace all the '&memory[HL()]' calls
+    if (address == 0xFF00){
+        //joypad register
+        if((system_mem[0xFF00] & 0x20) == 0)
+        {
+            //action buttons selected
+            return ((system_mem[0xFF00] & 0xF0) | (buttons & 0x0F));
+        }
+        else if((system_mem[0xFF00] & 0x10) == 0)
+        {
+            //direction buttons selected
+            return ((system_mem[0xFF00] & 0xF0) | (directions & 0x0F));
+        }
+    }
+    else if (address < 0x100 && system_mem[0xFF50] == 0x00)
+    {
+        return bootrom[address];
+    }
+    else //todo: more banking shiz
+        return system_mem[address];
+}
+
+void memory::performDMAtransfer(uint8_t data) {
+    uint16_t address = data << 8;
+    for (int i = 0; i < 0xA0; i++)
+    {
+        write_mem(0xFE00+i, read_mem(address+i));
+    }
+}
+
+void memory::LoadROM(const char *filename) {
+    //open file as binary stream and move file pointer to end
+    std::ifstream file(filename, std::ios::binary | std::ios::ate);
+
+    if(file.is_open())
+    {
+        //get size of file and allocate a buffer to hold the contents
+        std::streampos size = file.tellg();
+        char* buffer = new char[size];
+
+        //go back to beginning of file and fill buffer
+        file.seekg(0, std::ios::beg);
+        file.read(buffer, size);
+        file.close();
+
+        //load ROM contents into the gb's memory, starting at 0x00
+        for (int i = 0; i < size; i++)
+        {
+            cartridge_rom[i] = buffer[i];
+        }
+
+        //free the buffer
+        delete[] buffer;
+
+        //copy the first 0x8000 bytes into RAM
+        for (int i = 0; i < size; i++)
+        {
+            system_mem[i] = cartridge_rom[i];
+        }
+    }
+}
---
+++ memory.h
@@ -0,0 +1,29 @@
+//
+// Created by joshua on 3/29/22.
+//
+
+#ifndef GBEMUJM_MEMORY_H
+#define GBEMUJM_MEMORY_H
+
+
+#include <cstdint>
+#include <cstdio>
+#include <fstream>
+
+class memory {
+    uint8_t system_mem [0x10000]{};    //64KiB of memory
+    uint8_t bootrom [256]{}; //256 byte bootstrap
+    uint8_t cartridge_rom [0x200000]{}; //cartridge rom
+public:
+    uint8_t buttons = 0xFF;
+    uint8_t directions = 0xFF;
+    memory();
+
+    void write_mem(uint16_t address, uint8_t value);
+    uint8_t read_mem(uint16_t address);
+    void performDMAtransfer(uint8_t data);
+    void LoadROM(char const *filename);
+};
+
+
+#endif //GBEMUJM_MEMORY_H
